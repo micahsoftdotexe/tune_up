45843322e27bc912d6f0131361c2822c8ff07b37
micahsoftdotexe <micahrocz777@gmail.com>
2024-04-03 21:37:24 -0400

    remove old auth rules and permissions

--- priv/repo/migrations/20240404010234_remove_old_tables.exs
+++ priv/repo/migrations/20240404010234_remove_old_tables.exs
@@ -2,6 +2,10 @@ defmodule TuneUp.Repo.Migrations.RemoveOldTables do
   use Ecto.Migration
 
   def change do
-
+    # drop foreign_key(:auth_assignment, name: :auth_assignment_user_id_fkey)
+    drop table(:auth_item), mode: :cascade
+    drop table(:auth_assignment), mode: :cascade
+    drop table(:auth_item_child), mode: :cascade
+    drop table(:auth_rule), mode: :cascade
   end
 end
