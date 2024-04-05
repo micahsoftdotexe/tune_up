47ad8e71f939e6ec3716496797863f11f4e1406e
micahsoftdotexe <micahrocz777@gmail.com>
2024-04-04 20:19:59 -0400

    fixed schema to use public and rename migrations table

--- priv/repo/migrations/20240404010234_remove_old_tables.exs
+++ priv/repo/migrations/20240404010234_remove_old_tables.exs
@@ -3,6 +3,9 @@ defmodule TuneUp.Repo.Migrations.RemoveOldTables do
 
   def change do
     # drop foreign_key(:auth_assignment, name: :auth_assignment_user_id_fkey)
+    if Ecto.Adapters.SQL.table_exists?(TuneUp.Repo, "migration") do
+      rename table("migration"),  to: table(:yii_migrations)
+    end
     drop table(:auth_item), mode: :cascade
     drop table(:auth_assignment), mode: :cascade
     drop table(:auth_item_child), mode: :cascade
