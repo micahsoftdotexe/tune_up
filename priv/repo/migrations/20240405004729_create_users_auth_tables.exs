10a6fe43955a9d1b060282f5dea8d4b4af27fa38
micahsoftdotexe <micahrocz777@gmail.com>
2024-04-05 20:28:04 -0400

    edit users table and add emails to existing users

---
+++ priv/repo/migrations/20240405004729_create_users_auth_tables.exs
@@ -0,0 +1,36 @@
+defmodule TuneUp.Repo.Migrations.CreateUsersAuthTables do
+  use Ecto.Migration
+
+  def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
+    # create table(:users) do
+    #   add :email, :citext, null: false
+    #   add :hashed_password, :string, null: false
+    #   add :confirmed_at, :naive_datetime
+    #   timestamps(type: :utc_datetime)
+    # end
+    alter table(:users) do
+      add :email, :citext
+      add :confirmed_at, :naive_datetime
+      remove :auth_key
+      # timestamps(type: :utc_datetime)
+      timestamps(type: :utc_datetime, null: true)
+    end
+    rename table(:users), :password, to: :hashed_password
+
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
+
+    create table(:users_tokens) do
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(updated_at: false)
+    end
+
+    create index(:users_tokens, [:user_id])
+    create unique_index(:users_tokens, [:context, :token])
+  end
+end
---
+++ priv/repo/migrations/20240405124515_null_safe_email.exs
@@ -0,0 +1,21 @@
+defmodule TuneUp.Repo.Migrations.NullSafeEmail do
+  use Ecto.Migration
+  import Ecto.Query
+
+  def change do
+    # add emails to users
+    query = from u in "users", where: is_nil(u.email), select: {u.id, u.username}
+    if Enum.count(TuneUp.Repo.all(query)) > 0 do
+      results = TuneUp.Repo.all(query)
+      Enum.each(results, fn user -> add_email(user) end)
+    end
+    # add not null to emails
+    execute "ALTER TABLE users ALTER COLUMN email SET NOT NULL"
+  end
+
+  def add_email(user) do
+    {id, username} = user
+    email = IO.gets("Enter email for #{username}: ")
+    execute "UPDATE users SET email = '#{email}' WHERE id = #{id}"
+  end
+end
